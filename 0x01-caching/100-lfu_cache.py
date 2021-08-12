f01e20839a49b850a344126cd37b0ec6e4fc2aae
lilieyen <lnyasita@gmail.com>
2021-08-12 06:05:20 +0300

    lfu chaching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,47 +1,82 @@
-#!/usr/bin/env python3
-""" LFU cache"""
+#!/usr/bin/python3
+""" 5. LFU Caching
+"""
+
+from enum import Enum
+from heapq import heappush, heappop
+from itertools import count
+
+BaseCaching = __import__("base_caching").BaseCaching
 
 
-from collections import deque
-BaseCaching = __import__('base_caching').BaseCaching
+class HeapItemStatus(Enum):
+    """ HeapItemStatus class.
+    """
+    ACTIVE = 1
+    INACTIVE = 2
 
 
 class LFUCache(BaseCaching):
-    """ LFU caching algorithm """
+    """ Create a class LFUCache that inherits from BaseCaching and is a caching
+    system
+    """
 
     def __init__(self):
-        """ constructor """
+        """ Init
+        """
         super().__init__()
-        self.queue = []
-
-    @staticmethod
-    def getDictionaryValue(dict_i):
-        """ return the value of a key """
-        dict_key = list(dict_i.keys())[0]
-        return dict_i[dict_key]
+        self.heap = []
+        self.map = {}
+        self.counter = count()
 
     def put(self, key, item):
-        """ assign caching key to the item listed """
+        """ Must assign to the dictionary self.cache_data the item value for
+        the key key
+        """
         if key and item:
-            if len(self.cache_data) < self.MAX_ITEMS:
-                self.cache_data[key] = item
-                self.queue.append({key: 0})
+            if key in self.cache_data:
+                self.rehydrate(key)
             else:
-                self.queue.sort(reverse=True,
-                                key=self.getDictionaryValue)
-                popped = self.queue.pop()
-                dict_key = list(popped.keys())[0]
-                del self.cache_data[dict_key]
-                print("DISCARD: " + str(dict_key))
+                if self.is_full():
+                    self.evict()
+                self.add_to_heap(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ get the item attached with a key """
+        """ Must return the value in self.cache_data linked to key.
+        """
         if key in self.cache_data:
-            i = 0
-            for items in self.queue:
-                key_it = list(items.keys())
-                if key_it[0] == key:
-                    newElement = {key: items[key_it[0]] + 1}
-                    self.queue[i] = newElement
-                i += 1
-            return self.cache_data.get(key, None)
+            self.rehydrate(key)
+            return self.cache_data.get(key)
+
+    def is_full(self):
+        """ If the number of items in self.cache_data is higher that
+        BaseCaching.MAX_ITEMS
+        """
+        return len(self.cache_data) >= self.MAX_ITEMS
+
+    def evict(self):
+        """ you must print DISCARD: with the key discarded and following by a
+        new line
+        """
+        while self.heap:
+            _, __, item, status = heappop(self.heap)
+            if status == HeapItemStatus.ACTIVE:
+                print("DISCARD: " + str(item))
+                del self.cache_data[item]
+                return
+
+    def rehydrate(self, key):
+        """ Marks current item as inactive and reinserts updated count back
+        into heap.
+        """
+        entry = self.map[key]
+        entry[-1] = HeapItemStatus.INACTIVE
+        self.add_to_heap(key, entry[0])
+
+    def add_to_heap(self, key, count=0):
+        """ Adds a new entry into heap.
+        """
+        entry = [1 + count, next(self.counter), key, HeapItemStatus.ACTIVE]
+        self.map[key] = entry
+        heappush(self.heap, entry)
