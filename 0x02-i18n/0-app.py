c4c4dce297b62e8240f191ed7dd12d67a70d428e
lilieyen <lnyasita@gmail.com>
2021-08-18 04:38:30 +0300

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,9 +10,10 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"], strict_slashes=False)
 def hello() -> str:
     """
-    return a string
+    return string
     """
     return render_template('0-index.html')
 
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", port="5000")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
